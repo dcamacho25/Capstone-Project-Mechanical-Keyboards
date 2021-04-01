a42f30a09b92d20e30c38727d02fa616c208a0f8
Daniel Camacho <daniel_camacho_1@yahoo.com>
2021-04-01 19:51:53 -0400

    Fix issue on func hideMe and clean up some css

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+function hideMe() {
+    const hideElement = document.getElementById("more-info");
+    if (hideElement.style.display === "none") {
+        hideElement.style.display = "block";
+    } else {
+        hideElement.style.display = "none";
+    }
+}
+
+
 function submitForm(event) {
     event.preventDefault();
-    const something = document.getElementById('favswitch').value;
+    const userInput = document.getElementById('favswitch').value;
     var hasNumber = /\d/;
-    if (something === ""){
-        document.getElementById("answer").innerHTML = "Please type a response";
-    } else if (hasNumber.test(something)){
-        document.getElementById("answer").innerHTML = something;
+    if (userInput === ""){
+        document.getElementById("wrong-answer").innerHTML = "Please type a response";
+    } else if (hasNumber.test(userInput)){
+        document.getElementById("wrong-answer").innerHTML = "Please don't include numbers in your response";
+    } else {
+        document.getElementById("answer").innerHTML = "Thanks for using my site I'm glad  " +userInput+ " is your favorite.";
     }
 }
